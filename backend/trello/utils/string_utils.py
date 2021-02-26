fc3a2fea34f5a68a88de54dda5943e9763885269
ed <edchapman88@gmail.com>
2021-02-26 19:38:43 +0000

    Working Trello Board extract to DB.

--- backend/trello/utils/string_utils.py
+++ backend/trello/utils/string_utils.py
@@ -5,8 +5,13 @@ def get_est(text: str) -> str:
     """
     Extract estimate from text by matching numbers between brackets.
     """
-    match = re.findall('\(*[.?0-9 ]+\)', text)
-    if match:
-        match = match[0]
-        match = match.strip("( )")
-    return match or ""
+    est = None
+    try:
+        match = re.findall('\(*[.0-9 ]+\)', text)
+        if match:
+            est = match[0]
+            est = est.strip("( )")
+            est = float(est)
+    except Exception:
+        est = None
+    return est
