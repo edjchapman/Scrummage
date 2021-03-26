b1cbcb207ff1c7bcea0abdf61e37608483c1453c
ed <edchapman88@gmail.com>
2021-03-26 11:52:29 +0000

    Update get_est util function to extract values from either () or [] bracket styles (i.e. estimate and consumed extra).

--- backend/trello/utils/string_utils.py
+++ backend/trello/utils/string_utils.py
@@ -1,16 +1,17 @@
 import re
 
 
-def get_est(text: str) -> str:
+def get_est(text, bracket_style="()"):
     """
     Extract estimate from text by matching numbers between brackets.
     """
     est = None
+    pattern = "\\[[0-9.]*\\]" if bracket_style == "[]" else "\\([0-9.]*\\)"
     try:
-        match = re.findall('\(*[.0-9 ]+\)', text)
+        match = re.findall(pattern, text)
         if match:
             est = match[0]
-            est = est.strip("( )")
+            est = est.strip(bracket_style)
             est = float(est)
     except Exception:
         est = None
