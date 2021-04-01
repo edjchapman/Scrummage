8587239e2e0a6835e08893c46c065515fe032c47
ed <edchapman88@gmail.com>
2021-04-01 09:45:59 +0100

    Remove STATICFILES_DIRS from settings.

--- backend/scrummage/partial_settings/static_media.py
+++ backend/scrummage/partial_settings/static_media.py
@@ -4,7 +4,6 @@ from django.conf import settings
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(settings.OUTSIDE_PROJECT_DIR, 'static')
-STATICFILES_DIRS = ["templates/static"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(settings.OUTSIDE_PROJECT_DIR, 'media')
