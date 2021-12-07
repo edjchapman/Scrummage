16c51c1b81e475404158fef3a174e4f8baa452af
ed <edchapman88@gmail.com>
2021-12-07 00:58:56 +0000

    Move keys to environment variables.

---
+++ backend/scrummage/partial_settings/trello.py
@@ -0,0 +1,4 @@
+import os
+
+TRELLO_KEY = os.getenv("TRELLO_KEY")
+TRELLO_TOKEN = os.getenv("TRELLO_TOKEN")
--- backend/scrummage/settings.py
+++ backend/scrummage/settings.py
@@ -8,7 +8,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from .partial_settings.vars import *
 from .partial_settings.allowed_hosts import *
 from .partial_settings.apps import *
 from .partial_settings.celery import *
@@ -17,7 +16,9 @@ from .partial_settings.logging import *
 from .partial_settings.middleware import *
 from .partial_settings.password_validators import *
 from .partial_settings.rest_framework import *
+from .partial_settings.smtp import *
 from .partial_settings.static_media import *
 from .partial_settings.templates import *
 from .partial_settings.timezone import *
-from .partial_settings.smtp import *
+from .partial_settings.trello import *
+from .partial_settings.vars import *
